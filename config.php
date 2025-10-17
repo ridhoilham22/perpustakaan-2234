4b1caa8bb1e0eef44bae7d91b1750c7648c708ca
ridhoilham22 <2411500076@mahasiswa.atmaluhur.ac.id>
2025-10-17 14:41:35 +0700

    config.php

--- config.php
+++ config.php
@@ -7,10 +7,12 @@ $db_pass = '';
 $db_name 'db_perpustakaan';
 
 // mematikan error reporting untuk mysqli
-//mysqli_report
+//mysqli_report(MYSQLI_REPORT_OFF);
 
+//MEMBUAT KONEKSI
 $mysqli = nem mysqli($db_host, $db_user, $db_pass, $db_name);
 
+//ERROR HANDLING
 if ($mysqli ->conncet_error){
     die("koneksi gagal: " . $mysqli->connect_error);
 }
